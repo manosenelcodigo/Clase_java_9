d62f04285f9e3ab78337e2567a49e1ab6144ee92
#manosenelcódigo <peligrosamente@gmail.com>
2016-06-16 23:06:39 -0300

    Ejecutar.java

---
+++ Ejecutar.java
@@ -0,0 +1,14 @@
+package vistas;
+
+import graficas.Marco;
+
+
+public class Ejecutar {
+    
+    public static void main(String[] args) 
+    {
+        Marco marco=new Marco();
+        marco.getVentana();
+    }
+    
+}
